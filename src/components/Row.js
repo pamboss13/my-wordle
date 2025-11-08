271548f344bddd4bc3aadbac51ac488fbd61ac4b
pamboss13 <pambossvr@gmail.com>
2025-11-08 13:58:57 +0200

    Added styling

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,31 @@
-import styles from './Row.module.css'
+const Row = ({ guess, currentGuess }) => {
 
-const Row = () => {
+	if (guess) {
+		return (
+			<div className='row past'>
+				{guess.map((l, i) => (
+					<div key={i} className={l.color}>{l.key}</div>
+				))}
+			</div>
+		)
+	}
+
+	if (currentGuess) {
+		// split word in individual letters array
+		let letters = currentGuess.split('')
+
+		return (
+			<div className="row current">
+				{letters.map((letter, i) => (
+					<div key={i} className="filled">{letter}</div>
+				))}
+				{[...Array(5 - letters.length)].map((_, i) => (
+					<div key={i}></div>
+				))}
+			</div>
+		)
+	}
+
 	return (
 		<>
 			<div className="row">
